c49ab80c65a25b96e187e97107c8d8bf70ad940a
zeirany <zeiranurul11@gmail.com>
2025-10-30 17:24:46 +0700

    pertemuan4

--- luaslingkaran.php
+++ luaslingkaran.php
@@ -3,12 +3,22 @@
 class luaslingkaran {
 
     public const phi = 3.14;
-    public int $jari;
-}
+    public int$jari;
 
+    public function tampil($nama = 'ban') {
+        $rumus = luaslingkaran::phi * ($this->jari * $this->jari);
+        echo "lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+}
 
 $lingkaran = new luaslingkaran();
 $lingkaran->jari = 12;
+$lingkaran->tampil('roda');//panggil method tampil
 
-$rumus = luaslingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "hasilnya adalah: ".$rumus;
+luaslingkaran::testing();//panggil static method
+
